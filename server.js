67c09cb4ca7183d39243e4b419882f6b8b3e14aa
sufi <sufiyanshaikh11919@gmail.com>
2022-10-09 18:01:46 -0700

    express server running

--- server.js
+++ server.js
@@ -1,1 +1,21 @@
 // express server
+const express = require('express');
+const path = require('path');
+const PORT = 3001;
+// const uuid = require('./helpers/uuid');
+const fs = require('fs')
+const app = express();
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+app.use(express.static('public'));
+
+app.get('/', (req, res) =>
+  res.sendFile(path.join(__dirname, '/public/index.html'))
+);
+
+
+app.listen(PORT, () => {
+    console.log(`Server available at localhost${PORT}`);
+  });
